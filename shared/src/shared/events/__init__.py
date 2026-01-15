ac0f2537df593873f87cca03b6ea2c6d546e3c39
Henryk03 <enricocontessi03@gmail.com>
2026-01-15 15:06:11 +0100

    Added a new event

--- shared/src/shared/events/__init__.py
+++ shared/src/shared/events/__init__.py
@@ -3,6 +3,7 @@ from typing import Union
 
 from shared.events.chat import ChatMessageEvent
 from shared.events.error import ErrorEvent
+from shared.events.init import InitMetadataEvent
 from shared.events.auth import (
     LoginCompletedEvent,
     LoginFailedEvent,
@@ -11,6 +12,7 @@ from shared.events.auth import (
 
 
 Event = Union[
+    InitMetadataEvent,
     ChatMessageEvent,
     LoginRequiredEvent,
     LoginCompletedEvent,
