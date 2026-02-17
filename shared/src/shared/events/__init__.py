07715620d1092fccf4ef0fe30951355f4efb6aa0
Henryk03 <enricocontessi03@gmail.com>
2026-02-17 10:04:47 +0100

    Added the new event into the init

--- shared/src/shared/events/__init__.py
+++ shared/src/shared/events/__init__.py
@@ -1,6 +1,7 @@
 
 from typing import Union
 
+from shared.events.transport import EventEnvelope
 from shared.events.chat import ChatMessageEvent
 from shared.events.error import ErrorEvent
 from shared.events.clean import (
@@ -15,6 +16,7 @@ from shared.events.login import (
 
 
 Event = Union[
+    EventEnvelope,
     ChatMessageEvent,
     LoginRequiredEvent,
     LoginResultEvent,
