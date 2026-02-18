9e5e321db73c80bd86d66a67e94f4c335989e64a
Henryk03 <enricocontessi03@gmail.com>
2026-02-18 20:50:45 +0100

    Moved some imports into the right place

--- shared/src/shared/shared_utils/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from shared.shared_utils.common.dictionaries import AvailabilityDict
-from shared.shared_utils.common.enums import JobStatus
---
+++ shared/src/shared/shared_utils/common/__init__.py
@@ -0,0 +1,3 @@
+
+from shared.shared_utils.common.dictionaries import AvailabilityDict
+from shared.shared_utils.common.enums import JobStatus
